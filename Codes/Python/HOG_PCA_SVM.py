eaec9c7ccdc5924fbeae8fab449ca9ce9c245910
Wai Po Kevin Teng <37227666+twpkevin06222@users.noreply.github.com>
2018-05-24 17:24:55 +0200

    Update HOG_PCA_SVM.py
    The basic idea of the algorithm for HOG + SVM + 10-Fold Cross Validation

--- Codes/Python/HOG_PCA_SVM.py
+++ Codes/Python/HOG_PCA_SVM.py
@@ -3,11 +3,10 @@ import glob
 import numpy as np 
 import cv2
 from sklearn import svm
-#from sklearn.cross_validation import StratifiedKFold
 from sklearn.metrics import confusion_matrix
-from sklearn.cross_validation import StratifiedKFold
+from sklearn.model_selection import RepeatedKFold
 import matplotlib.pyplot as plt
-#from mlxtend.evaluate import confusion_matrix
+
 
 os.chdir('C:/Users/blaze03/Desktop/tmp')
 list_fams = os.listdir(os.getcwd()) # vector of strings with family names
@@ -44,6 +43,10 @@ cell_size = (8, 8)
 num_bins = 9
 hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, num_bins)
 
+# =============================================================================
+# X = np.zeros((sum(no_imgs),320)) # Feature Matrix
+# cnt = 0
+# =============================================================================
 x_pos=[]
 for i in range(len(list_fams)):
     os.chdir(list_fams[i])
@@ -55,56 +58,29 @@ for i in range(len(list_fams)):
     os.chdir('..')
 x_pos = np.array(x_pos, dtype=np.float32)
 x_new01 = x_pos.reshape(x_pos.shape[0], x_pos.shape[1])
-print(x_new01.shape)
+print(x_new01.shape) #with the matrix (no of images, features per image)
 # =============================================================================
-# Dimension Reduction 
+# cross validation 
 # =============================================================================
-x_t = x_new01.transpose()
-mu, eig = cv2.PCACompute(x_t, np.array([]))
-x_pca = cv2.PCAProject(x_t,mu,eig)
-# =============================================================================
-# Cross Validation + Classification 
-# =============================================================================
-#cross validate
-kfold = 10
-#clf= svm.SVC(kernel='rbf', gamma=0.7) #SVM classifier
-clf= svm.SVC(kernel='linear', C=1)
-
-skf = StratifiedKFold(y,kfold)
-skfind = [None]*len(skf) # indices
-cnt=0
-for train_index in skf:
-  skfind[cnt] = train_index
-  cnt = cnt + 1
-
-x_p = x_pca  #change the value for x 
+C_p = 1.5
+clf= svm.SVC(kernel='linear', C= C_p)
+#clf= svm.SVC(kernel='rbf', gamma=0.7)
 conf_mat = np.zeros((len(no_imgs),len(no_imgs)))
-#k-fold cross validation 
-for i in range(kfold):
-    train_indices = skfind[i][0]
-    test_indices = skfind[i][1]
-    X_train = x_p[train_indices]
-    y_train = y[train_indices]
-    X_test = x_p[test_indices]
-    y_test = y[test_indices]  
-    clf.fit(X_train,y_train)  #Training    
-    y_predict = clf.predict(X_test) # output is labels and not indices  #Testing      
-    cm = confusion_matrix(y_test,y_predict)  # Compute confusion matrix
+#kf = KFold(n_splits=10)
+rkf = RepeatedKFold(n_splits = 10, n_repeats = 10)
+for train, test in rkf.split(x_new01):
+    X_train, X_test, y_train, y_test = x_new01[train], x_new01[test], y[train], y[test]
+    clf.fit(X_train,y_train)
+    y_predict = clf.predict(X_test)
+    cm = confusion_matrix(y_test,y_predict)
     conf_mat = conf_mat + cm
-     
-conf_mat = conf_mat.T # since rows and  cols are interchanged
-avg_acc = np.trace(conf_mat)/sum(no_imgs)
-conf_mat_norm = conf_mat/no_imgs # Normalizing the confusion matri
 
-# =============================================================================
-# Plot confusion matrix 
-# =============================================================================
+conf_mat = conf_mat.T # since rows and  cols are interchanged
+avg_acc = np.trace(conf_mat)/(10*sum(no_imgs))
+conf_mat_norm = np.array(conf_mat)/(np.array(no_imgs, dtype =int)*10) # Normalizing the confusion matrix
 print('Computing Confusion Matrix')
 plt.imshow(conf_mat_norm, interpolation='nearest')
 plt.title('Confusion matrix')
 plt.colorbar()
 plt.show()
-#plt.savefig('confusion_matrix.png')
-
-
-
+print('Accuracy of the method is', avg_acc)
